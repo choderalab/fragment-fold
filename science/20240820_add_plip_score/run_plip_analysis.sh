9c8c91ebb961787e73cca1e09dca85f70ec0e3fa
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-11-20 14:19:27 -0500

    update bash submission script

--- science/20240820_add_plip_score/run_plip_analysis.sh
+++ science/20240820_add_plip_score/run_plip_analysis.sh
@@ -1,5 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
+
+#BSUB -oo logs/plip_analysis.out
+#BSUB -eo logs/plip_analysis.stderr
+#BSUB -n 1
+#BSUB -q cpuqueue
+#BSUB -R rusage[mem=32]
+#BSUB -W 24:00
 
 python3 run_plip_analysis.py \
---structure-dir /lila/data/chodera/asap-restricted/broad-spectrum-validation_maria/sars_mers_gen_validation/sars_matching_mers \
---output-dir
+--yaml_input interaction_datasets.yaml \
+--output-dir /lila/data/chodera/asap-restricted/broad-spectrum-validation_maria/sars_mers_gen_validation/20241120_plip_analysis
