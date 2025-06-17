07fd46d5cdd447393e82dd374fc0f61827c1e724
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-06-17 19:13:58 -0400

    add updated plip analysis

---
+++ science/20240820_add_plip_score/20250617_plip_analysis/run_analysis.sh
@@ -0,0 +1,5 @@
+source ~/.bashrc
+python3 /Users/alexpayne/Scientific_Projects/broad-spectrum-asap-paper/scripts/calculate_plip_interactions.py \
+--yaml-input 20250617.yaml \
+--output-dir interactions_results \
+--ncpus 8
